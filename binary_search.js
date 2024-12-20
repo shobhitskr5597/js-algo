b6c694c334063a86a1c57a6050db9b99a8d51a9d
Shobhit Kumar <shobhitskr5597@gmail.com>
2024-12-20 16:19:25 +0530

    First commit binary search

---
+++ binary_search.js
@@ -0,0 +1,19 @@
+function binarySearch(arr,numb) {
+    let leftIndex = 0;
+    let rightIndex = arr.length -1;
+    
+    while (leftIndex<= rightIndex) {
+        let middleIndex = Math.floor((leftIndex + rightIndex)/2)
+      if (arr[middleIndex] === numb) {
+          return middleIndex
+      }
+      if (numb < arr[middleIndex]) {
+          rightIndex = middleIndex-1;
+      } else {
+          leftIndex = middleIndex+1;
+      }
+    }
+    return -1
+  } 
+  
+  console.log(binarySearch([-5,2,4,5,7,8,10],7))
