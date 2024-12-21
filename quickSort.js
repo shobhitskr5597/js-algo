07e697bb156627f5994bf699d822f63f94b90071
Shobhit Kumar <shobhitskr5597@gmail.com>
2024-12-21 15:49:50 +0530

    Quick Sort

---
+++ quickSort.js
@@ -0,0 +1,20 @@
+function quickSort(arr) {
+    if (arr.length < 2) {
+      return arr
+    }
+  
+    let left = []
+    let right = []
+    let pivot = arr[arr.length - 1]
+    for (let i = 0; i < arr.length-1; i++) {
+      if (arr[i] < pivot) {
+        left.push(arr[i])
+      } else {
+        right.push(arr[i])
+      }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)]
+  }
+  
+  console.log(quickSort([-5, 8, 3, 7, 4, 0, 6]))
+  
